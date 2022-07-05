f22b660bfc710eed4b1620b688bd77a34de70664
hokageCV <chaitanyavaru@gmail.com>
2022-07-05 12:27:14 +0530

    initial commit

---
+++ src/components/testing.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Testing extends React.Component {
+    render(){
+        return(
+            <div className = "bokuto">
+                <h2> RASENGAN</h2>
+                <p> hey hey hey </p>
+            </div>
+        );
+    }
+}
+
+export {Testing};
